0119d512cb091dad80cfa708b3da2337bea14da1
DeanM <mdean317@gmail.com>
2025-04-14 21:53:42 -0400

    login form

--- components/LoginForm/LoginForm.jsx
+++ components/LoginForm/LoginForm.jsx
@@ -1,15 +1,58 @@
 const LoginForm = (props) => {
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        props.setUser(true);
+
+    const handleSubmit = async (event) => {
+        try {
+
+            event.preventDefault();
+
+            
+            const Response = await fetch("http://localhost:3000/auth/sign-in", {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    accountName: event.target.elements.accountName.value,
+                    userName: event.target.elements.userName.value,
+                    password: event.target.elements.password.value
+                })
+              })
+
+            if (Response) {
+
+                event.target.elements.accountName.value = '';
+                event.target.elements.userName.value = '';
+                event.target.elements.password.value = '';
+                
+                const signedInUser = await Response.json();
+               
+                props.setUser(signedInUser);
+            }
+
+        } catch (err) {
+
+            console.log(err);
+
+        }
     }
 
     return (
-        <form onSubmit={handleSubmit}>
+        <form action="http://localhost:3000/auth/sign-in" method="POST" onSubmit={handleSubmit}>
+
             <p>login form</p>
-            <button type='submit'>Submit!</button>
+            <label for="accountName">Account Name:</label>
+            <input type="text" name="accountName" id="accountName" required />
+
+            <label for="userName">User Name:</label>
+            <input type="text" name="userName" id="userName" required />
+
+            <label for="password">Password:</label>
+            <input type="password" name="password" id="password" required />
+
+            <button type="submit" id="signUpButton">Submit! </button>
         </form>
     );
+
 };
 
 export default LoginForm;
