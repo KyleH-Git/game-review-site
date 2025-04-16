6262eb8c8ff777f36c8af260fa665ede30137d07
JohnNg-GA <john.q.ng@gmail.com>
2025-04-16 11:35:26 -0700

    Review Form CSS Styling

--- components/ReviewForm/ReviewForm.jsx
+++ components/ReviewForm/ReviewForm.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import './ReviewForm.css'
 
 const ReviewForm = ({setGameData, gameData, setPage, setUserGameReview, user}) => {
 
@@ -64,47 +65,49 @@ const ReviewForm = ({setGameData, gameData, setPage, setUserGameReview, user}) =
             <div className='game-container' key={gameData.gameId}>
                 <img src={gameData.gameImg} style={{ maxWidth: 500}}/>
                 <h2>{gameData.gameName}</h2>
-                <p>Overall Rating: {gameData.gameRating}</p>
-                <p>Date Released: {gameData.gameReleased}</p>
-                <div id='genreBox'> 
-                    <p>Genre: {gameData.gameGenre.map((genre) => (
-                        genre.name + ' '
-                    ))}
-                    </p>
+                <h3>Overall Rating: <span style={{fontWeight: 'normal'}}>{gameData.gameRating}</span></h3>
+                <h3>Date Released: <span style={{fontWeight: 'normal'}}>{gameData.gameReleased}</span></h3>
+                <h3>Genre: {gameData.gameGenre.map((genre) => (
+                    <span style={{fontWeight: 'normal'}}>{genre.name + ' '}</span>
+                ))}
+                </h3>
             </div>
-        </div>
-            <form>
-                <label name="title">Review Title: 
+            <form className="reviewForm">
+                <label name="title" style={{fontSize: '2em'}}>Review Title: </label><br/>
                     <input
+                    id='revTitle'
                     type="text"
                     name='title'
                     placeholder={`${user.userName}'s review`}
                     value={newReview.title}
                     onChange={handleChange}
                     />
-                </label><br/><br/>
-                <label name="body">Review:<br/>
+                <br/><br/>
+                <label name="body" style={{fontSize: '2em'}}>Your Review:<br/>
                     <textarea 
-                    rows="6"
-                    cols="50"
+                    rows="10"
+                    cols="70"
                     type="text"
                     name='body'
                     value={newReview.body}
                     placeholder="Type Review Here"
+                    style={{padding: '10px', fontSize: '14px'}}
                     onChange={handleChange}>
                     </textarea>
                 </label><br/><br/>
                 <label name="stars">
-                    <button name='stars' value={1} onClick={handleChange}>1 Star</button>
-                    <button name='stars' value={2} onClick={handleChange}>2 Stars</button>
-                    <button name='stars' value={3} onClick={handleChange}>3 Stars</button>
-                    <button name='stars' value={4} onClick={handleChange}>4 Stars</button> 
-                    <button name='stars' value={5} onClick={handleChange}>5 Stars</button> 
+                    <div id="starsBtn">
+                        <button name='stars' value={1} onClick={handleChange}>1 Star</button>
+                        <button name='stars' value={2} onClick={handleChange}>2 Stars</button>
+                        <button name='stars' value={3} onClick={handleChange}>3 Stars</button>
+                        <button name='stars' value={4} onClick={handleChange}>4 Stars</button> 
+                        <button name='stars' value={5} onClick={handleChange}>5 Stars</button> 
+                    </div>
                 </label>
                 <br/><br/>
                 <button onClick={handleSubmit}>Submit New Review</button>
             </form>
-            <div><a href="https://rawg.io/" target="_blank"> Powered by RAWG.io</a></div>
+            <a id='apiRef' href="https://rawg.io/" target="_blank"> Powered by RAWG.io</a>
         </div>
         
         );
